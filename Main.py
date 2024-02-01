4599fc9b1e4f94c5bf634a466a9610fcdaae0716
T55 <86412779+T55sArt@users.noreply.github.com>
2024-01-31 19:40:32 -0800

    New Main
    Added RLE vs RLCE char saving comparison n such

--- Main.py
+++ Main.py
@@ -1,14 +1,39 @@
 import RunLengthChangedEncoding as rlce
 
-data = ['a', 'a', 'a', 'a', 'a', ]
+#Input:
+data = ['a', 'a', 'b', 'b', 'a', 'b', 'c', 'd', 'd', '3#s', '3', '4', '5_t', '7#e', '7#e', '#1', '2#']
 delimiter = '#'
 
-print('Original: ' + str(data) + '\nDelimiter: ' + delimiter)
-print('')
-print('Parse Example: 3#a = ' + str(rlce.ParseRLE("3#a" , delimiter)))
-print('')
+#Processed by RLCE:
 encodedData = rlce.Encode(data, delimiter)
-print('Encoded: ' + str(encodedData) + '\nData Saved: ' + str(round(100 - ((len(encodedData) / len(data)) * 100), 3)) + '%, Orginal Count: ' + str(len(data)) + ', Encoded Count: ' + str(len(encodedData)))
-print('')
 decodedData = rlce.Decode(str, encodedData, delimiter)
-print('Decoded: ' + str(decodedData) + '\nDecode Success: ' + str(data == decodedData))
+
+#RLE vs RLCE:
+traditionalRLE = []
+prev = str(data[0])
+runLen = 0
+for i in data:
+    i = str(i)
+    if i == prev:
+        runLen = runLen + 1
+    else:
+        traditionalRLE.append(f'{runLen}{delimiter}{prev}')
+        prev = i
+        runLen = 1
+traditionalRLE.append(f'{runLen}{delimiter}{prev}')
+
+#Summing up chars count from RLE & RLCE:
+beforeChars = 0
+for i in traditionalRLE:
+    beforeChars = beforeChars + len(i)
+afterChars = 0
+for i in encodedData:
+    afterChars = afterChars + len(i)
+
+#Statistics:
+print(f'Original: {data}\nDelimiter: {delimiter}\n')
+print(f'Parse Example: 3#a = {rlce.ParseRLE("3#a", delimiter)}\n')
+print(f'Encoded: {encodedData}')
+print(f'Data Saved (Count Wise): {round(100-((len(encodedData)/len(data))*100),3)}%, Orginal Count: {len(data)}, Encoded Count: {len(encodedData)}')
+print(f'Data Saved (RLE vs RLCE (Char Wise)): {round(100-((afterChars/beforeChars)*100),3)}%, RLE Count: {beforeChars}, RLCE Count: {afterChars}\n')
+print(f'Decoded: {decodedData}\nDecode Success: {data == decodedData}')
